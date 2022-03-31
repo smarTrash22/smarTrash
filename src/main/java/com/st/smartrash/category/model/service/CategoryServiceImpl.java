6696e869ea3b0e3d24f21898283d304e3f048458
Sinion <sinion16@naver.com>
2022-04-01 02:40:09 +0900

    category fix

--- src/main/java/com/st/smartrash/category/model/service/CategoryServiceImpl.java
+++ src/main/java/com/st/smartrash/category/model/service/CategoryServiceImpl.java
@@ -3,10 +3,12 @@ package com.st.smartrash.category.model.service;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.st.smartrash.category.model.dao.CategoryDao;
 import com.st.smartrash.category.model.vo.Category;
 
+@Service("categoryService")
 public class CategoryServiceImpl implements CategoryService {
 	
 	@Autowired
