7d331cb0dd04fffaa968b970e96f5bf40bf67587
songsangeon <zxva717@naver.com>
2022-04-01 17:32:59 +0900

    04.01 송상언 보드 모델생성및 메퍼 생성 네비에서 갤러리 클릭 .do 경로변경

---
+++ src/main/java/com/st/smartrash/board/model/service/BoardServiceImpl.java
@@ -0,0 +1,69 @@
+package com.st.smartrash.board.model.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.st.smartrash.board.model.dao.BoardDao;
+import com.st.smartrash.board.model.vo.Board;
+import com.st.smartrash.common.Paging;
+
+
+@Service("boardService")
+public class BoardServiceImpl implements BoardService {
+	@Autowired
+	private BoardDao boardDao;
+	
+
+	@Override
+	public int selectListCount() {
+		return boardDao.selectListCount();
+	}
+
+	@Override
+	public ArrayList<Board> selectList(Paging page) {
+		return boardDao.selectList(page);
+	}
+
+	@Override
+	public Board selectBoard(int board_num) {
+		return boardDao.selectBoard(board_num);
+	}
+
+	@Override
+	public int updateAddReadcount(int board_num) {
+		return boardDao.updateAddReadcount(board_num);
+	}
+
+	@Override
+	public int insertOriginBoard(Board board) {
+		return boardDao.insertOriginBoard(board);
+	}
+
+	@Override
+	public int insertReply(Board reply) {
+		return boardDao.insertReply(reply);
+	}
+
+	@Override
+	public int updateReplySeq(Board reply) {
+		return boardDao.updateReplySeq(reply);
+	}
+
+	@Override
+	public int updateOrigin(Board board) {
+		return boardDao.updateOrigin(board);
+	}
+
+	@Override
+	public int updateReply(Board reply) {
+		return boardDao.updateReply(reply);
+	}
+
+	@Override
+	public int deleteBoard(Board board) {
+		return boardDao.deleteBoard(board);
+	}
+
+}
